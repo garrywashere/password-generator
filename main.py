6a964e9a20ecd8c6ae2cba547adc625387b45b73
Garry <github@garrynet.co.uk>
2024-02-08 00:03:32 +0000

    fixed character checking

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 
 import random, string
 
-qwerty = "qwertyuiopasdfghjklzxcvbnm"
 lower_char_bank = string.ascii_lowercase
 upper_char_bank = string.ascii_uppercase
 digi_bank = string.digits
@@ -13,13 +12,17 @@ all = lower_char_bank + upper_char_bank + digi_bank + spec_bank
 
 chars = int(input("How many characters? "))
 
+if chars < 8:
+    chars = 8
+
 lower_char_count, upper_char_count, digi_count, spec_count = 0,0,0,0
 
-while lower_char_count < 1 and upper_char_count < 1 and digi_count < 1 and spec_count < 1:
+running = True
+while running:
     password = []
 
     for x in range(chars):
-        password.append(all[random.randint(0, len(all))])
+        password.append(all[random.randint(0, len(all)-1)])
     
     for char in password:
         if char in lower_char_bank:
@@ -30,5 +33,9 @@ while lower_char_count < 1 and upper_char_count < 1 and digi_count < 1 and spec_
             digi_count +=1
         elif char in spec_bank:
             spec_count +=1
+    
+    if lower_char_count > 1 and upper_char_count > 1 and digi_count > 1 and spec_count > 1:
+        running = False
             
+password = "".join(password)
 print(password)
